8493ab23a0c518e50807236b02c58e50fc812d69
Marina <marinal2303@yandex.ru>
2022-10-14 15:50:53 +0300

    правки

--- source/js/promo-carousel.js
+++ source/js/promo-carousel.js
@@ -1,22 +1,22 @@
-
+/* Секция с нужными элементами, чтобы быстрее искать */
 const promo = document.querySelector('.promo');
-
+/* Кнопка прокрутки карусели товаров - назад */
 const promoButtonPrev = promo.querySelector('.promo__button-prev');
-
+/* Кнопка прокрутки карусели товаров - вперед */
 const promoButtonNext = promo.querySelector('.promo__button-next');
-
+/* Список товаров */
 const promoList = promo.querySelector('.promo__list');
-
+/* Общее количество товаров */
 const promoCount = promo.querySelectorAll('.promo__item').length;
-
+/* Одна карточка */
 const promoItem = promo.querySelector('.promo__item');
-
+/* Ширина одной карточки товара */
 const productWidth = promoItem.clientWidth;
 
 
 let count = 0;
 
-
+/* Измерям на сколько px нужно пролистывать */
 const setPosition = () => {
   promoList.style.transform = `translateX(${-count*productWidth}px)`;
 }
